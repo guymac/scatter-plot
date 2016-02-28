e28217a408c20adb082416f73d82d288143175dc
Kairi Kozuma <kairikozuma@gmail.com>
2016-02-28 13:48:02 -0500

    Create a point table module
    - Create PointTable class with table of points
    - Add two textfield objects for x and y input
    - Add a point generator button that generates number of points chosen
    on the spinner
    - Remove the size constraints on the scene
    - Pass data from the PointPlotter class to instantiate the PointTable
    class as a module

--- src/program/PointPlotter.java
+++ src/program/PointPlotter.java
@@ -1,15 +1,24 @@
 package program;
 
+import model.Point;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 /**
  * Java FX application that plots points
  * @author Kairi Kozuma
  * @version 1.0
  */
 public class PointPlotter extends Application {
+
+    // ObservableList of Points
+    private ObservableList<Point> data;
+
+    // Table module
+    private PointTable tableModule;
 
     // Must override this method for class that extends Application
     @Override
@@ -18,8 +27,17 @@ public class PointPlotter extends Application {
         // Root node
         Group root = new Group();
 
+        // Initialize the observable list
+        data = FXCollections.observableArrayList();
+
+        // Initialize instance of table module
+        tableModule = new PointTable(data);
+
+        // Add the module view to root node
+        root.getChildren().addAll(tableModule.getView());
+
         // Container associated with root node
-        Scene scene = new Scene(root, 400, 300); // Size of window 400 x 300
+        Scene scene = new Scene(root);
 
         // Set the scene for the stage
         stage.setScene(scene);
